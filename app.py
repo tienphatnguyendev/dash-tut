3dbb52359e09b8ac7d7000b1198c1a8cf0663c9a
Aaron Nguyen <aaronnguyen@Aarons-Air-2.modem>
2024-04-16 22:03:43 +1000

    init project

---
+++ app.py
@@ -0,0 +1,24 @@
+from dash import Dash, html, dash_table, dcc
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
+
+app = Dash(__name__)
+server = app.server  # add this line
+
+app.layout = html.Div([
+    html.Div(children='My First App with Data and a Graph'),
+    dash_table.DataTable(
+        data=df.to_dict('records'), 
+        page_size=10
+    ),
+    dcc.Graph(
+        figure=px.histogram(
+            df, x='continent', y='lifeExp', histfunc='avg'
+        )
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
